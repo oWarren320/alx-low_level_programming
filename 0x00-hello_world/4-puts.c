e62586c82f46db9d51088182263438c47e09300c
warren <ochiengwarren320@gmail.com>
2022-12-13 09:38:26 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,3 +5,12 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *
+ *    * Return: 0
+ *
+ *     */
