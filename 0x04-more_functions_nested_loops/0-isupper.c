6afd39285e7c40d6ab6861bad17c72dc9e987136
warren <ochiengwarren320@gmail.com>
2022-12-16 05:00:46 -0800

    ccheck supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return : Always 0.
+ */
+
+int _isupper(int c);
+{
+	char alpha []= {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	if c == alpha;
+	return (1);
+	else ;
+	{
+		return (0)
+	}
+}
+
+
+int main(void)
+{
+	char c;
+	
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Talking about functions and nested loops
