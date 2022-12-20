f6a5e90483c9d885d93e99596445ddd6741debd1
imaginethree0 <ochiengwarren320@gmail.com>
2022-12-20 18:06:22 +0300

    updated 0-reset file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * void reset_to_98 - change the value of n to 98 
- * @n - integer to change to 
+ * void reset_to_98 - change the value of n to 98
+ * @n - integer to change to
  *
- * Return nothing 
+ * Return nothing
  */
 
 void reset_to_98(int *n)
