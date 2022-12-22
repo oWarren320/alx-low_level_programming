b5ed234c2a00dd871429f7b5fc289d40d713b019
imaginethree0 <ochiengwarren320@gmail.com>
2022-12-22 22:38:14 +0300

    update 0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,5 +8,19 @@
 
 char *_strcat (char *dest, char *src)
 {
-	strcat( dest , src);
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
 }
